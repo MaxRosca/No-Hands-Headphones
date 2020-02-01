91a9bf8a20f3ebbaedd2cb92bf7818a1d0ce311e
MaxRosca <roscamaxim2002@gmail.com>
2020-02-01 00:01:22 -0800

    Added few things that i dont remember

--- app/src/main/java/com/example/nohandsheadphonesapp/ConnectThread.kt
+++ app/src/main/java/com/example/nohandsheadphonesapp/ConnectThread.kt
@@ -1,55 +1,40 @@
 
 import android.bluetooth.BluetoothSocket
-import android.bluetooth.BluetoothDevice
-import java.util.UUID.randomUUID
+import android.os.Handler
+import android.os.Message
 import android.util.Log
+import android.widget.TextView
 import java.io.IOException
 import java.util.*
 
-class ConnectThread(private val mmDevice: BluetoothDevice?) : Thread() {
-    private val mmSocket: BluetoothSocket?
+class ConnectThread(private val mmSocket: BluetoothSocket?, val handler: Handler, val textV: TextView) : Thread() {
+    val TAG = "ConnectThreadTAG"
     var MY_UUID = UUID.fromString("0000112f-0000-1000-8000-00805f9b34fb")
 
-    init {
-        // Use a temporary object that is later assigned to mmSocket,
-        // because mmSocket is final
-        var tmp: BluetoothSocket? = null
-
-        // Get a BluetoothSocket to connect with the given BluetoothDevice
-        try {
-            // MY_UUID is the app's UUID string, also used by the server code
-            tmp = mmDevice?.createInsecureRfcommSocketToServiceRecord(MY_UUID)
-        } catch (e: IOException) {
-
-            Log.d("CONNECTTHREAD", "Could not close connection:" + e.toString())
-        }
-
-        mmSocket = tmp
-    }
-
     override fun run() {
-        // Cancel discovery because it will slow down the connection
-        //mBluetoothAdapter.cancelDiscovery();
         try {
-            // Connect the device through the socket. This will block
-            // until it succeeds or throws an exception
-            mmSocket!!.connect()
-            var inputStream = mmSocket.inputStream
-            var outputStream = mmSocket.outputStream
-            val mmBuffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
-            var numBytes: Int // bytes returned from read()
+            Log.i(TAG, "Connected to socket ! ")
+            var inputStream = mmSocket?.inputStream
+            var outputStream = mmSocket?.outputStream
+            val mmBuffer: ByteArray = ByteArray(1024)
+            var numBytes: Int
 
             while (true) {
-                // Read from the InputStream.
-                numBytes = try {
-                    inputStream.read(mmBuffer)
+                Log.v("sfas", "gi")
+
+                try {
+                    numBytes = inputStream!!.read(mmBuffer)
+//                    Log.v(TAG, "HI")
                 } catch (e: IOException) {
-                    Log.v("Appinfo", "Input stream was disconnected", e)
+                    Log.v(TAG, "Input stream was disconnected", e)
                     break
                 }
-                Log.v("Appinfo"," measa" + numBytes.toString())
 
+                handler.post {
+                    val s = String(mmBuffer, 0, numBytes)
 
+                    textV.text = s
+                }
             }
 
         } catch (connectException: IOException) {
--- app/src/main/java/com/example/nohandsheadphonesapp/FullscreenActivity.kt
+++ app/src/main/java/com/example/nohandsheadphonesapp/FullscreenActivity.kt
@@ -3,12 +3,20 @@ package com.example.nohandsheadphonesapp
 import ConnectThread
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.view.View.*
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.*
 
 
 class FullscreenActivity : AppCompatActivity() {
@@ -19,9 +27,21 @@ class FullscreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_fullscreen)
-        val button: Button = findViewById(R.id.button)
+        window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                SYSTEM_UI_FLAG_FULLSCREEN or SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                SYSTEM_UI_FLAG_LAYOUT_STABLE or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
-        button.setOnClickListener {
+        val connectButton: Button = findViewById(R.id.connectButton)
+        val sendButton: Button = findViewById(R.id.sendButton)
+        val sendMessageEditText = findViewById<EditText>(R.id.sendMessage)
+        val recievedText = findViewById<TextView>(R.id.recievedMessages)
+        var connected = false
+        var connThread: ConnectThread?
+        var inputStream: InputStream? = null
+        var outputStream: OutputStream? = null
+        val mainHandler = Handler()
+
+        connectButton.setOnClickListener {
 
             val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
@@ -30,28 +50,53 @@ class FullscreenActivity : AppCompatActivity() {
                 startActivityForResult(enableBtIntent, 2)
             }
 
-            var myDevice: BluetoothDevice? = null
+            // Huawei - 5C:A8:6A:89:1E:70
+            // J5 - BC:54:51:07:62:C5
+            // Note 9 - F4:C2:48:E7:63:B6
 
-            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
-            pairedDevices?.forEach { device ->
+            var myDevice: BluetoothDevice? = bluetoothAdapter?.getRemoteDevice("F4:C2:48:E7:63:B6")
+
+            Log.i(TAG, myDevice?.name)
+//            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+//            pairedDevices?.forEach { device ->
 //                Log.v(TAG, device.name + " " + device.address)
-
-                if (device.name == "Galaxy J5 2017"){
-                    myDevice = device
-//                    Log.v(TAG, myDevice?.name)
-                }
-            }
-
-            val connThread = ConnectThread(myDevice)
+//
+//                if (device.name == "Galaxy J5 2017"){
+//                    myDevice = device
+////                    Log.v(TAG, myDevice?.name)
+//                }
+//            }
 
             try {
-                connThread.run()
+                var MY_UUID = UUID.fromString("0000112f-0000-1000-8000-00805f9b34fb")
+
+                try {
+                    var mySocket = myDevice?.createInsecureRfcommSocketToServiceRecord(MY_UUID)
+
+                    if(mySocket?.isConnected != true) {
+                        mySocket!!.connect()
+                    }
+
+                    inputStream = mySocket.inputStream
+                    outputStream = mySocket.outputStream
+                    connThread = ConnectThread(mySocket, mainHandler, recievedText)
+                    connThread!!.start()
+
+                } catch (e: IOException) {
+                    Log.d(TAG, "Could not close connection:" + e.toString())
+                }
             }
-            catch (e: IOException){
+            catch (e: Exception){
                 Log.d(TAG, e.toString())
 
             }
 
+        }
+
+        sendButton.setOnClickListener {
+            if(connected){
+                outputStream?.write(sendMessageEditText.text.toString().toByteArray())
+            }
         }
     }
 }
